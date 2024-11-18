658c75d55acbf9564675759b39f0044c52676b4f
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-11-18 10:56:48 -0300

    Add table html structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,28 @@ function App() {
    return (
       <>
          <h1>Paginação com React</h1>
+         <table>
+            <thead>
+               <tr>
+                  <th>Código</th>
+                  <th>Nome</th>
+                  <th>E-mail</th>
+               </tr>
+            </thead>
+            <tbody>
+               <tr>
+                  <td>48123</td>
+                  <td>Fulano de Tal</td>
+                  <td>fulanodetal@emailfake.com.br</td>
+               </tr>
+            </tbody>
+            <tfoot>
+               <tr>
+                  <td colSpan={1}>10 de 228</td>
+                  <td colSpan={2}>Página 1 de 23</td>
+               </tr>
+            </tfoot>
+         </table>
       </>
    );
 }
