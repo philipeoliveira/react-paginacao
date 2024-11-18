e2e81ada347ad22b282678f3253f8e94ef6346fa
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-11-18 12:51:47 -0300

    Create pagination buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from 'lucide-react';
+
 function App() {
    return (
       <div className='max-w-6xl mx-auto flex flex-col gap-6 p-6'>
@@ -30,10 +32,38 @@ function App() {
                         10 de 228
                      </td>
                      <td
-                        colSpan={2}
+                        colSpan={20}
                         className='py-2 px-3 text-zinc-400 text-sm text-right'
                      >
-                        Página 1 de 23
+                        <div className='flex items-center justify-end gap-4'>
+                           <span>Página 1 de 23</span>
+                           <div className='flex gap-0.5'>
+                              <button
+                                 type='button'
+                                 className='bg-zinc-400 border border-zinc-950 text-zinc-950 rounded-lg p-1 hover:bg-zinc-300'
+                              >
+                                 <ChevronsLeft size={18} />
+                              </button>
+                              <button
+                                 type='button'
+                                 className='bg-zinc-400 border border-zinc-950 text-zinc-950 rounded-lg p-1 hover:bg-zinc-300'
+                              >
+                                 <ChevronLeft size={18} />
+                              </button>
+                              <button
+                                 type='button'
+                                 className='bg-zinc-400 border border-zinc-950 text-zinc-950 rounded-lg p-1 hover:bg-zinc-300'
+                              >
+                                 <ChevronsRight size={18} />
+                              </button>
+                              <button
+                                 type='button'
+                                 className='bg-zinc-400 border border-zinc-950 text-zinc-950 rounded-lg p-1 hover:bg-zinc-300'
+                              >
+                                 <ChevronRight size={18} />
+                              </button>
+                           </div>
+                        </div>
                      </td>
                   </tr>
                </tfoot>
