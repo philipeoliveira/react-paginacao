43391b94f912e4c058f28361221abb74abac4164
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-12-07 17:18:11 -0300

    Add title to pagination buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,10 @@
-export function Button({ children, onClick, disabled }) {
+export function Button({ children, onClick, title, disabled }) {
    return (
       <button
          type='button'
-         className='bg-zinc-400 border border-zinc-950 text-zinc-950 rounded-lg p-1 hover:bg-zinc-300 disabled:bg-zinc-700'
+         className='bg-zinc-400 border border-zinc-950 text-zinc-950 rounded-lg p-1.5 hover:bg-zinc-300 disabled:bg-zinc-700'
          onClick={onClick}
+         title={title}
          disabled={disabled}
       >
          {children}
